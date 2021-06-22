d0374040fc98f5fd07e923e571e2e791e2604659
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2021-06-22 11:17:03 -0300

    Entity User

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,32 @@
-import {Entity} from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid'
 
-@Entity()
-export class User {
+@Entity('users')
+class User {
 
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admim: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
 }
+
+export default User
