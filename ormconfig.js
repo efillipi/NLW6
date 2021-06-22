38c3fda467a5241e41cd4be4f39e3cdb46d2f6d5
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2021-06-22 10:23:33 -0300

    first commit

--- ormconfig.js
+++ ormconfig.js
@@ -7,9 +7,6 @@ dotevnt.config({
 module.exports = {
   "type": process.env.DB_CLIENT,
   "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE,
   "entities": [process.env.TYPEORM_ENTITIES],
   "migrations": [process.env.TYPEORM_MIGRATION],
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 import express from 'express';
+import './database'
 
 const app = express();
 
 app.use(express.json());
-
 
 export default app
